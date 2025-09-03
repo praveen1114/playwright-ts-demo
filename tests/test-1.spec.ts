66cdca0e46de7c909f99c9eabadbf34cc532f6a8
Your Name <you@example.com>
2025-09-03 17:22:48 +0530

    iframe

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,12 +1,23 @@
 import {test, expect} from '@playwright/test';
 
 test ('locator demo',async({page}) => {
-await page.goto('http://127.0.0.1:5500/tests/locators.html');
-const clickButton = page.getByRole('button',{name:'Click Me'})
-// console.log(clickButton);
-await clickButton.click();
-const nav = page.getByRole('navigation',{name:'Main Navigation'})
-console.log(nav);
-await nav.isVisible;
+// // await page.goto('http://127.0.0.1:5500/tests/locators.html');
+// const clickButton = page.getByRole('button',{name:'Click Me'})
+// // console.log(clickButton);
+// await clickButton.click();
+// const nav = page.getByRole('navigation',{name:'Main Navigation'})
+// console.log(nav);
+// await nav.isVisible;
+
+//Locator chaining & iframe html
+await page.goto('http://127.0.0.1:5500/tests/iframeapp.html');
+
+// Accessing a frame first
+const frame = page.frameLocator('#loginFrame');
+
+await frame.getByLabel('Username').fill('myUser')
+// Narrowing down inside the frame
+await frame.getByLabel('Password').fill('myPassword');
+
 
 })
